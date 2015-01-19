718f996af511386f1003f6645e87bce7fa55876f
Pablo Nevares <pablo@nevares.com>
2015-01-19 15:46:56 -0700

    caching so good

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var config = require('./config')(),
     url = require('url'),
     http = require('http'),
     twitter = require('twitter'),
+    redis = require('redis'),
     winchatty = require('./winchatty');
 
-var client = new twitter(config.api_keys);
+var redisClient = redis.createClient(),
+    twitterClient = new twitter(config.api_keys);
 
 var server = http.createServer(function(req, res) {
     var output;
@@ -23,41 +25,64 @@ var server = http.createServer(function(req, res) {
             }
 
             var tweetId = tweetUrl.match(/twitter\.com\/\w+\/status\/(\d+).*/);
+
             if(tweetId === null || tweetId.length !== 2) {
                 res.writeHead(400, {'Content-Type': 'text/html'});
                 res.end('400 Bad Request<br>Could not find tweetId');
                 break;
             }
 
-            console.log('validating ' + tweetUrl);
-            winchatty.search(tweetId[1], function(error, response) {
+            tweetId = tweetId[1];
+
+            redisClient.hget(tweetId, 'body', function(error, response) {
                 if(error) {
-                    console.log('winchatty module error: ' + error);
                     res.writeHead(500, {'Content-Type': 'text/html'});
-                    res.end('500 Winchatty Error');
-                    return;
-                }
-
-                if(response.posts.length === 0) {
-                    console.log('no posts found containing ' + tweetUrl);
-                    res.writeHead(400, {'Content-Type': 'text/html'});
-                    res.end('400 Bad Request<br>No chatty post found with this Twitter url');
-                    return;
-                }
-
-                console.log('retrieving ' + tweetUrl);
-                client.get('statuses/oembed.json', {url: tweetUrl}, function(error, params, response) {
-                    if(error) {
-                        console.log('twitter module error:' + error);
-                        res.writeHead(500, {'Content-Type': 'text/html'});
-                        res.end('500 Internal Server Error');
-                    } else {
-                        output = params.html || 'error';
-                    }
-
+                    res.end('500 Internal Server Error');
+                } else if(response !== null) {
+                    // cache hit, use it
+                    console.log('cache hit for ' + tweetId);
                     res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.end(output);
-                });
+                    res.end(response);
+
+                    redisClient.hincrby(tweetId, 'requests', 1);
+                } else {
+                    // cache miss, get it
+                    console.log('cache miss for ' + tweetId);
+                    console.log('validating ' + tweetUrl);
+                    winchatty.search(tweetId, function(error, response) {
+                        if(error) {
+                            console.log('winchatty module error: ' + error);
+                            res.writeHead(500, {'Content-Type': 'text/html'});
+                            res.end('500 Winchatty Error');
+                            return;
+                        }
+
+                        if(response.posts.length === 0) {
+                            console.log('no posts found containing ' + tweetUrl);
+                            res.writeHead(400, {'Content-Type': 'text/html'});
+                            res.end('400 Bad Request<br>No chatty post found with this Twitter url');
+                            return;
+                        }
+
+                        console.log('retrieving ' + tweetUrl);
+                        twitterClient.get('statuses/oembed.json', {url: tweetUrl}, function(error, params, response) {
+                            if(error) {
+                                console.log('twitter module error:' + error);
+                                res.writeHead(500, {'Content-Type': 'text/html'});
+                                res.end('500 Internal Server Error');
+                            } else {
+                                output = params.html || 'error';
+                            }
+
+                            res.writeHead(200, {'Content-Type': 'text/html'});
+                            res.end(output);
+
+                            // save in cache
+                            console.log('saving in redis: ' + tweetId);
+                            redisClient.hmset(tweetId, 'body', output, 'requests', 1);
+                        });
+                    });
+                }
             });
             break;
         default:
@@ -69,4 +94,8 @@ var server = http.createServer(function(req, res) {
 server.listen(config.port, function() {
     console.log(new Date().toISOString());
     console.log('twitter-reader began listening on port ' + config.port);
+});
+
+redisClient.on('error', function(error) {
+    console.log('redis error: ' + error);
 });
--- winchatty.js
+++ winchatty.js
@@ -21,4 +21,4 @@ module.exports = {
           callback(e.message, null);
         });
     }
-}
+};
