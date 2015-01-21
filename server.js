dfd5f9cf3c006d983fc31a962313b7bbff9e9589
Pablo Nevares <pablo@nevares.com>
2015-01-21 14:52:53 -0700

    callbackHell--

--- server.js
+++ server.js
@@ -11,12 +11,13 @@ var redisClient = redis.createClient(),
     twitterClient = new twitter(config.api_keys);
 
 var server = http.createServer(function(req, res) {
-    var parsed = url.parse(req.url, true);
+    var dateStamp, tweetUrl, tweetId,
+        parsed = url.parse(req.url, true);
 
     switch(parsed.pathname) {
         case '/tweet':
-            var dateStamp = moment().utc().format('YYYY-MM-DD');
-            var tweetUrl = parsed.query.tweetUrl || '';
+            dateStamp = moment().utc().format('YYYY-MM-DD');
+            tweetUrl = parsed.query.tweetUrl || '';
 
             redisClient.zincrby('daily-requests', 1, dateStamp);
 
@@ -25,7 +26,7 @@ var server = http.createServer(function(req, res) {
                 return;
             }
 
-            var tweetId = tweetUrl.match(/twitter\.com\/\w+\/status\/(\d+).*/);
+            tweetId = tweetUrl.match(/twitter\.com\/\w+\/status\/(\d+).*/);
 
             if(tweetId === null || tweetId.length !== 2) {
                 outputResponse(res, 400, '400 Bad Request<br>Could not find tweetId');
@@ -34,63 +35,69 @@ var server = http.createServer(function(req, res) {
 
             tweetId = tweetId[1];
 
-            redisClient.hget('tweet:' + tweetId, 'body', function(error, response) {
-                if(error) {
-                    outputResponse(res, 500, '500 Internal Server Error');
-                    return;
-                } else if(response !== null) {
-                    console.log('cache hit for tweet:' + tweetId);
-                    outputResponse(res, 200, response);
-                    redisClient.zincrby('daily-served', 1, dateStamp);
-                    redisClient.zincrby('tweet-requests', 1, 'tweet:' + tweetId);
-                } else {
-                    console.log('cache miss for tweet:' + tweetId);
-                    console.log('validating ' + tweetUrl);
-                    winchatty.search(tweetId, function(error, response) {
-                        if(error || response.posts.length === 0) {
-                            if(error) {
-                                console.log('winchatty module error: ' + error);
-                            }
-                            outputResponse(res, 400, '400 Bad Request<br>No chatty post found with this Twitter url');
-                            return;
-                        }
-
-                        console.log('retrieving ' + tweetUrl);
-                        twitterClient.get('statuses/oembed.json', {url: tweetUrl}, function(error, response) {
-                            redisClient.zincrby('daily-twitter-api', 1, dateStamp);
-
-                            if(error || !('html' in response)) {
-                                console.log('twitter module error:' + error);
-                                outputResponse(res, 500, '500 Internal Server Error');
-                                return;
-                            }
-
-                            var tweetBody = response.html;
-                            outputResponse(res, 200, tweetBody);
-
-                            console.log('caching tweet:' + tweetId);
-                            redisClient.hmset('tweet:' + tweetId, 'body', tweetBody, 'url', tweetUrl, 'saved', moment().utc().format());
-                            redisClient.zincrby('daily-served', 1, dateStamp);
-                            redisClient.zincrby('tweet-requests', 1, 'tweet:' + tweetId);
-                        });
-                    });
-                }
-            });
+            redisClient.hget('tweet:' + tweetId, 'body', loadFromCache);
             break;
         default:
             console.log('bad endpoint');
             outputResponse(res, 404, '404 Not Found<br>Unknown path: ' + parsed.pathname);
+    }
+
+    function loadFromCache(error, response) {
+        if(error) {
+            outputResponse(res, 500, '500 Internal Server Error');
+            return;
+        } else if(response !== null) {
+            console.log('cache hit for tweet:' + tweetId);
+            outputResponse(res, 200, response);
+            redisClient.zincrby('daily-served', 1, dateStamp);
+            redisClient.zincrby('tweet-requests', 1, 'tweet:' + tweetId);
+        } else {
+            console.log('cache miss for tweet:' + tweetId);
+            console.log('validating ' + tweetUrl);
+            winchatty.search(tweetId, validateUsingSearch);
+        }
+    }
+
+    function validateUsingSearch(error, response) {
+        if(error || response.posts.length === 0) {
+            if(error) {
+                console.log('winchatty module error: ' + error);
+            }
+            outputResponse(res, 400, '400 Bad Request<br>No chatty post found with this Twitter url');
+            return;
+        }
+
+        console.log('retrieving ' + tweetUrl);
+        twitterClient.get('statuses/oembed.json', {url: tweetUrl}, loadFromTwitter);
+    }
+
+    function loadFromTwitter(error, response) {
+        redisClient.zincrby('daily-twitter-api', 1, dateStamp);
+
+        if(error || !('html' in response)) {
+            console.log('twitter module error:' + error);
+            outputResponse(res, 500, '500 Internal Server Error');
+            return;
+        }
+
+        var tweetBody = response.html;
+        outputResponse(res, 200, tweetBody);
+
+        console.log('caching tweet:' + tweetId);
+        redisClient.hmset('tweet:' + tweetId, 'body', tweetBody, 'url', tweetUrl, 'saved', moment().utc().format());
+        redisClient.zincrby('daily-served', 1, dateStamp);
+        redisClient.zincrby('tweet-requests', 1, 'tweet:' + tweetId);
+    }
+
+    function outputResponse(res, httpCode, body) {
+        res.writeHead(httpCode, {'Content-Type': 'text/html'});
+        res.end(body);
     }
 });
 
 redisClient.on('error', function(error) {
     console.log('redis error: ' + error);
 });
-
-function outputResponse(res, httpCode, body) {
-    res.writeHead(httpCode, {'Content-Type': 'text/html'});
-    res.end(body);
-}
 
 server.listen(config.port, function() {
     console.log(moment().utc().format());
