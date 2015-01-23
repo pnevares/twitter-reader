2c5530246c25d691da78fba54f7b771075d0f33f
Pablo Nevares <pablo@nevares.com>
2015-01-23 02:48:18 -0700

    server and listener living in harmony

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 var config = require('./lib/config')(process.env);
 
-var tweetServer = new require('./lib/tweetServer')(config);
+var TweetServerModule = require('./lib/tweetServer');
+var tweetServer = new TweetServerModule(config);
 tweetServer.start();
+
+var ChattyListenerModule = require('./lib/chattyListener');
+var chattyListener = new ChattyListenerModule(config);
+chattyListener.start();
