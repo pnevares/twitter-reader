2abdc3d6490973b2c9f13450d7c23952d1855a7b
Pablo Nevares <pablo@nevares.com>
2015-01-31 05:15:51 -0700

    test case for mobile urls

--- spec/tweetScanner-spec.js
+++ spec/tweetScanner-spec.js
@@ -4,7 +4,8 @@ describe('The tweetScanner module', function() {
     var tweets = [
         'https://twitter.com/Interior/status/507185938620219395',
         'https://twitter.com/NASA/status/559796020406538240',
-        'HTTPS://TWITTER.COM/BarackObama/STATUS/266031293945503744'
+        'HTTPS://TWITTER.COM/BarackObama/STATUS/266031293945503744',
+        'https://mobile.twitter.com/BarackObama/status/266031293945503744',
     ];
 
     it('should handle empty input', function() {
@@ -35,6 +36,13 @@ describe('The tweetScanner module', function() {
         var result = tweetScanner(tweets[2]);
         expect(result.length).toEqual(1);
         expect(result[0]).not.toEqual(tweets[2]);
+        expect(result[0]).toEqual('https://twitter.com/BarackObama/status/266031293945503744');
+    });
+
+    it('should reformat urls to desktop site', function() {
+        var result = tweetScanner(tweets[3]);
+        expect(result.length).toEqual(1);
+        expect(result[0]).not.toEqual(tweets[3]);
         expect(result[0]).toEqual('https://twitter.com/BarackObama/status/266031293945503744');
     });
 });
